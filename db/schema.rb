2510e90493ddb6700d2a39d07fdc82289bd5427e
RidaAhmad <rida.ahmad@square63.com>
2016-10-10 03:12:33 +0500

    User: Added column for username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005095303) do
+ActiveRecord::Schema.define(version: 20161006061205) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20161005095303) do
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
     t.string   "access_token",           limit: 25
+    t.string   "username",               limit: 25,               null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
